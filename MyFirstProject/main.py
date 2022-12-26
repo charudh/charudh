05251b14ff352863e89a2600c64f8fbfc57aaeb2
charudh <95066194+charudh@users.noreply.github.com>
2022-12-26 18:33:56 +0530

    Updated main.py with arguments of user point on command line

--- MyFirstProject/main.py
+++ MyFirstProject/main.py
@@ -1,6 +1,11 @@
 import turtle
 from random import randint
 import Geometry_game
+import sys
+
+# total arguments
+argNum = (len(sys.argv))-1
+
 
 # Create rectangle object
 rectangle= Geometry_game.GraphicRectangle(Geometry_game.Point(randint(0, 200), randint(0, 200)),
@@ -18,19 +23,20 @@ myturtle= turtle.Turtle()
 rectangle.draw(myturtle)
 
 
+if argNum == 2 :
+    # get point from user
+    #user_point = Geometry_game.GraphicPoint(float(input("Guess X : ")),float(input("Guess Y : ")))
+    user_point = Geometry_game.GraphicPoint(float(sys.argv[1]), float(sys.argv[2]))
 
-# get point from user
-#user_point = Geometry_game.GraphicPoint(float(input("Guess X : ")),float(input("Guess Y : ")))
-user_point = Geometry_game.GraphicPoint(150.0,200.0)
+    # print the game result
+    if user_point.falls_in_rectangle(rectangle):
+        print("Your point is inside the rectangle")
+    else:
+        print("Your point is outside the rectangle")
 
-# print the game result
-if user_point.falls_in_rectangle(rectangle):
-    print("Your point is inside the rectangle")
-else:
-    print("Your point is outside the rectangle")
+    user_point.draw(myturtle)
+
 
 print("Area of rectangle : ", rectangle.area())
-
-user_point.draw(myturtle)
 
 turtle.done()
